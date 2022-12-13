7c84611a637f1731eea9077180f0d7cf60d472f2
Ralph <ralph_souza@hotmail.com>
2022-12-13 11:08:00 -0300

    Add Student and Registration models with choices

---
+++ apps/registration/migrations/0001_initial.py
@@ -0,0 +1,43 @@
+from django.db import migrations, models
+import django.db.models.deletion
+import django.utils.timezone
+import uuid
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Student',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True)),
+                ('name', models.CharField(max_length=200)),
+                ('id_doc', models.CharField(max_length=15)),
+                ('created_at', models.DateTimeField(default=django.utils.timezone.now)),
+            ],
+            options={
+                'verbose_name': 'Student',
+                'ordering': ('-created_at',),
+            },
+        ),
+        migrations.CreateModel(
+            name='Registration',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('email', models.EmailField(max_length=250)),
+                ('phone', models.CharField(max_length=15)),
+                ('gender', models.CharField(choices=[('male', 'Masculino'), ('female', 'Feminino'), ('other', 'Outro(a)')], default=None, max_length=50)),
+                ('degree', models.CharField(choices=[('fundamental', 'Fundamental completo'), ('fundamental_na', 'Fundamental incompleto'), ('high_school', 'Ensino medio completo'), ('high_school_na', 'Ensino medio incompleto'), ('graduation', 'Superior completo'), ('graduation_na', 'Superio incompleto'), ('other', 'Outro')], default=None, max_length=50)),
+                ('relationship', models.CharField(choices=[('father', 'Pai'), ('mother', 'Mae'), ('other', 'Outro')], default=None, max_length=50)),
+                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='registration.student')),
+            ],
+            options={
+                'verbose_name': 'Registration',
+            },
+        ),
+    ]
--- apps/registration/models.py
+++ apps/registration/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+from .choices import GENDER_CHOICES, DEGREE_CHOICES, RELATIONSHIP_CHOICES
+
+
+class Student(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200, blank=False, null=False)
+    id_doc = models.CharField(max_length=15, blank=False, null=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = "Student"
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return self.name
+
+
+class Registration(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=250, blank=False, null=False)
+    phone = models.CharField(max_length=15, blank=False, null=False)
+    gender = models.CharField(max_length=50, choices=GENDER_CHOICES, default=None)
+    degree = models.CharField(max_length=50, choices=DEGREE_CHOICES, default=None)
+    relationship = models.CharField(max_length=50, choices=RELATIONSHIP_CHOICES, default=None)
+
+    class Meta:
+        verbose_name = "Registration"
+
+    def __str__(self):
+        return self.student
