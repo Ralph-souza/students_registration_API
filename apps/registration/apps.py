8cd1a3633c64f550923c2707eaef925c34ec0848
Ralph <ralph_souza@hotmail.com>
2022-12-13 09:01:46 -0300

    Add application restructuring to apps repository

--- apps/registration/apps.py
+++ apps/registration/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RegistrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'registration'
+
+
+class CoreConfig(AppConfig):
+    name = 'registration'
